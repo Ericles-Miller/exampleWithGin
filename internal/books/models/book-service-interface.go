32f4f55d1b90ad085bfbba65d7aa1c32ed49f24d
Éricles Miller <ericlesmiller15@gmail.com>
2026-05-27 10:34:32 -0300

    feat: create service loan

--- internal/books/models/book-service-interface.go
+++ internal/books/models/book-service-interface.go
@@ -3,7 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type BookService interface {
-	CreateBook(book *Book) error
+	CreateBook(book *Book) (*Book, error)
 	GetBook(id uuid.UUID) (*Book, error)
 	GetAllBooks() ([]*Book, error)
 	UpdateBook(id uuid.UUID, book *Book) (*Book, error)
